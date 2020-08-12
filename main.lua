26c5de8347926bb345ac9c35b1de125bdc0cc0ba
Roberto Castro <robertocastroroy@gmail.com>
2020-08-12 16:24:40 +0200

    Add main file with game loop functions and no buffer print

---
+++ main.lua
@@ -0,0 +1,16 @@
+io.stdout:setvbuf("no")
+
+--require("mobdebug").on()
+love.window.setTitle("Game Title")
+
+function love.load()
+  
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  
+end  
